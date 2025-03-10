6412bddf24f84ad17a2abd7a6a73289008f78a30
HowardHoang <besthieu2002@gmail.com>
2025-03-10 15:49:33 +0700

    update

--- src/toDoList/TodoItem.js
+++ src/toDoList/TodoItem.js
@@ -4,15 +4,10 @@ export default class TodoItem extends Component {
     render() {
         return (
             <div>
-                <div style={{ backgroundColor: "#f9f9f9", display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '8px', borderBottom: "1px solid #ddd", marginBottom: '5px' }}>
-                    task 1
+                <div style={{ backgroundColor: "#f9f9f9", display: 'flex', flex: 1, justifyContent: 'space-between', alignItems: 'center', padding: '8px', borderBottom: "1px solid #ddd", marginBottom: '5px' }}>
+                    {this.props.taskList.content}
                 </div>
-                <div style={{ backgroundColor: "#f9f9f9", display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '8px', borderBottom: "1px solid #ddd", marginBottom: '5px' }}>
-                    task 2
-                </div>
-                <div style={{ backgroundColor: "#f9f9f9", display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '8px', borderBottom: "1px solid #ddd", marginBottom: '5px' }}>
-                    task 3
-                </div>
+
             </div>
 
         )
--- src/toDoList/TodoList.js
+++ src/toDoList/TodoList.js
@@ -1,16 +1,87 @@
 import React, { Component } from 'react'
+import './Todolist.css'
 import AddTodo from './AddTodo'
 import TodoItem from './TodoItem'
 
+
 export default class TodoList extends Component {
+
+    state = {
+        taskList: [
+            { id: 1, content: 'task 1', completed: false },
+            { id: 2, content: 'task 2', completed: true },
+            { id: 3, content: 'task 3', completed: false },
+            { id: 4, content: 'task 4', completed: true },
+        ],
+        newTask: '',
+        completed: false
+    }
+
+    renderTaskToDo = () => {
+        return this.state.taskList.filter(item => !item.completed).map((item, index) => {
+            return <li key={index}>
+                <TodoItem key={item.id} taskList={item} />
+                <div className="buttons">
+                    <button className="remove" type="button" >
+                        <i className="fa fa-trash-alt" />
+                    </button>
+                    <button type="button" className="complete" >
+                        <i className="far fa-check-circle" />
+                        <i className="fas fa-check-circle" />
+                    </button>
+                </div>
+            </li>
+        })
+    }
+
+    renderTaskToDoDone = () => {
+        return this.state.taskList.filter(item => item.completed).map((item, index) => {
+            return <li key={index}>
+                <TodoItem key={item.id} taskList={item} />
+                <div className="buttons">
+                    <button className="remove" type="button" >
+                        <i className="fa fa-trash-alt" />
+                    </button>
+                    <button type="button" className="complete" >
+                        <i className="far fa-undo" />
+                        <i className="fas fa-undo" />
+                    </button>
+                </div>
+            </li>
+        })
+    }
+
 
     render() {
         return (
-            <div className="todo-list" style={{ width: '20%', margin: 'auto', marginTop: '100px' }}>
-                <h3>Todo App</h3>
-                <AddTodo />
-                <TodoItem />
-            </div>
+            <form >
+                <div className="card">
+                    <div className="card__header">
+                    </div>
+                    <div className="card__body">
+                        <div className="card__content">
+                            <div className="form-group">
+                                <div className="card__title">
+                                    <h2>Todo App</h2>
+                                </div>
+                                <AddTodo />
+                                {/* <span className="text text-danger">{this.state.errors.taskName}</span> */}
+                            </div>
+
+                            <div className="card__todo form-group">
+                                {/* Uncompleted tasks */}
+                                <ul className="todo" id="todo">
+                                    {this.renderTaskToDo()}
+                                </ul>
+                                {/* Completed tasks */}
+                                <ul className="todo" id="completed">
+                                    {this.renderTaskToDoDone()}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
         )
     }
 }
