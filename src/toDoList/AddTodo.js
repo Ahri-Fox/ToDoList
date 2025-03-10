58c87c8b27d3ed9c5a03cfc2a0a81d7a29f13f97
HowardHoang <besthieu2002@gmail.com>
2025-03-10 17:20:49 +0700

    Create/ Edit/ Delete Todo

--- src/toDoList/AddTodo.js
+++ src/toDoList/AddTodo.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 
 export default class AddTodo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { taskName: "" };
+    }
+
+    handleChange = (event) => {
+        this.setState({ taskName: event.target.value });
+
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (this.state.taskName.trim() === "") return;
+
+        this.props.onAdd(this.state.taskName);
+        this.setState({ taskName: "" });
+    };
+
     render() {
         return (
             <div className="card__add">
-                <input name="taskName" id="newTask" type="text" placeholder="Enter an activity..." />
-                <button id="addItem" >
+                <input name="taskName" value={this.state.taskName} onChange={this.handleChange} id="newTask" type="text" placeholder="Add your new todo" />
+                <button onClick={this.handleSubmit} id="addItem" >
                     <i className="fa fa-plus" />
                 </button>
             </div>
--- src/toDoList/TodoList.js
+++ src/toDoList/TodoList.js
@@ -17,17 +17,54 @@ export default class TodoList extends Component {
         completed: false
     }
 
+    handleAdd = (content) => {
+        const newTask = {
+            id: Date.now(),
+            content,
+            completed: false
+        }
+        this.setState({
+            taskList: [...this.state.taskList, newTask],
+            newTask: ''
+        })
+    }
+
+    doneTask = (id) => {
+        this.setState({
+            taskList: this.state.taskList.map(item => item.id === id ? { ...item, completed: true } : item)
+        });
+    };
+
+    undoTask = (id) => {
+        this.setState({
+            taskList: this.state.taskList.map(item => item.id === id ? { ...item, completed: false } : item)
+        });
+    }
+    deleteTask = (id) => {
+        this.setState({
+            taskList: this.state.taskList.filter(item => item.id !== id)
+        });
+    }
+    clearAllTasks = () => {
+        this.setState({
+            taskList: []
+        });
+    }
+
     renderTaskToDo = () => {
         return this.state.taskList.filter(item => !item.completed).map((item, index) => {
             return <li key={index}>
                 <TodoItem key={item.id} taskList={item} />
                 <div className="buttons">
-                    <button className="remove" type="button" >
+                    <button onClick={() => {
+                        this.deleteTask(item.id)
+                    }} className="remove" type="button" >
                         <i className="fa fa-trash-alt" />
                     </button>
-                    <button type="button" className="complete" >
+                    <button onClick={() => {
+                        this.doneTask(item.id)
+                    }} type="button" className="complete" >
                         <i className="far fa-check-circle" />
-                        <i className="fas fa-check-circle" />
                     </button>
                 </div>
             </li>
@@ -39,12 +76,15 @@ export default class TodoList extends Component {
             return <li key={index}>
                 <TodoItem key={item.id} taskList={item} />
                 <div className="buttons">
-                    <button className="remove" type="button" >
+                    <button onClick={() => {
+                        this.deleteTask(item.id)
+                    }} className="remove" type="button" >
                         <i className="fa fa-trash-alt" />
                     </button>
-                    <button type="button" className="complete" >
-                        <i className="far fa-undo" />
-                        <i className="fas fa-undo" />
+                    <button onClick={() => {
+                        this.undoTask(item.id)
+                    }} type="button" className="complete" >
+                        <i class="fa fa-undo-alt"></i>
                     </button>
                 </div>
             </li>
@@ -64,7 +104,7 @@ export default class TodoList extends Component {
                                 <div className="card__title">
                                     <h2>Todo App</h2>
                                 </div>
-                                <AddTodo />
+                                <AddTodo newTask={this.state.newTask} onAdd={this.handleAdd} />
                                 {/* <span className="text text-danger">{this.state.errors.taskName}</span> */}
                             </div>
 
@@ -77,11 +117,20 @@ export default class TodoList extends Component {
                                 <ul className="todo" id="completed">
                                     {this.renderTaskToDoDone()}
                                 </ul>
+
+                                <ul className="todo d-flex justify-content-center" id="todo"  >
+                                    YOU HAVE {this.state.taskList.length} TASKS
+                                    <div>
+                                        <button onClick={() => { this.clearAllTasks() }} type='button' className=' btn btn-danger p-1 ml-1'>
+                                            Clear all
+                                        </button>
+                                    </div>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-            </form>
+            </form >
         )
     }
 }
